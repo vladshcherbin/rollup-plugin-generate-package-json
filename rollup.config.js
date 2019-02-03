e86053a4d2f267282c55afe2fbfab699b3fd1f3b
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2019-02-03 16:10:51 +0300

    Set node target version to 8, add esm build

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,20 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
-  },
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm'
+    }
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**',
-      presets: [['@babel/preset-env', { targets: { node: 6 }, modules: false }]]
+      presets: [['@babel/preset-env', { targets: { node: 8 } }]]
     })
   ],
   external: ['path', 'read-pkg', 'write-pkg']
