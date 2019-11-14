4aa9981768421973e36477e5659da717643d218c
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2019-11-15 01:05:38 +0300

    Remove input package.json bad format test

--- tests/fixtures/src/input-file-bad-format/index.js
+++
@@ -1,9 +0,0 @@
-import Koa from 'koa'
-
-const app = new Koa()
-
-app.use((ctx) => {
-  ctx.body = 'Hey'
-})
-
-app.listen(3000)
--- tests/index.test.js
+++ tests/index.test.js
@@ -29,215 +29,206 @@ afterEach(async () => {
   await fs.remove('dist')
 })
 
-describe('Input package.json', () => {
-  test('throw if doesn\'t exist', async () => {
-    await expect(build({ inputFolder: 'src/input-file-absent' }))
-      .rejects.toThrow('Input package.json file does not exist or has bad format, check "inputFolder" option')
+test('throw if input package.json doesn\'t exist', async () => {
+  await expect(build({ inputFolder: 'src/input-file-absent' }))
+    .rejects.toThrow('Input package.json file does not exist or has bad format, check "inputFolder" option')
+})
+
+test('throw if not possible to save generated file', async () => {
+  await expect(build({
+    inputFolder: 'src/output-bad-format',
+    outputFolder: 'src/output-bad-format/folder'
+  })).rejects.toThrow('Unable to save generated package.json file, check "outputFolder" option')
+})
+
+test('no options', async () => {
+  process.chdir(`${__dirname}/fixtures/src/dependencies`)
+
+  const bundle = await rollup({
+    input: 'index.js',
+    plugins: [
+      generatePackageJson()
+    ],
+    external: ['koa']
   })
 
-  test('throw if has bad format', async () => {
-    await expect(build({ inputFolder: 'src/input-file-bad-format' }))
-      .rejects.toThrow('Input package.json file does not exist or has bad format, check "inputFolder" option')
+  await bundle.write({
+    file: path.resolve(process.cwd(), '../../dist/app.js'),
+    format: 'cjs'
+  })
+
+  process.chdir(`${__dirname}/fixtures`)
+
+  await expect(readDistPackageJson()).resolves.toEqual({
+    dependencies: {
+      koa: '2.0'
+    }
   })
 })
 
-describe('Generate package.json file', () => {
-  test('throw if not possible to save generated file', async () => {
-    await expect(build({
-      inputFolder: 'src/output-bad-format',
-      outputFolder: 'src/output-bad-format/folder'
-    })).rejects.toThrow('Unable to save generated package.json file, check "outputFolder" option')
+test('no dependencies', async () => {
+  await build({ inputFolder: 'src/no-dependencies' })
+
+  await expect(readDistPackageJson()).resolves.toEqual({})
+})
+
+test('missing dependencies', async () => {
+  await build({ inputFolder: 'src/missing-dependencies' })
+
+  await expect(readDistPackageJson()).resolves.toEqual({})
+})
+
+test('dependencies', async () => {
+  await build({ inputFolder: 'src/dependencies' })
+
+  await expect(readDistPackageJson()).resolves.toEqual({
+    dependencies: {
+      koa: '2.0'
+    }
   })
+})
 
-  test('no options', async () => {
-    process.chdir(`${__dirname}/fixtures/src/dependencies`)
-
-    const bundle = await rollup({
-      input: 'index.js',
-      plugins: [
-        generatePackageJson()
-      ],
-      external: ['koa']
-    })
-
-    await bundle.write({
-      file: path.resolve(process.cwd(), '../../dist/app.js'),
-      format: 'cjs'
-    })
-
-    process.chdir(`${__dirname}/fixtures`)
-
-    await expect(readDistPackageJson()).resolves.toEqual({
-      dependencies: {
-        koa: '2.0'
-      }
-    })
-  })
-
-  test('no dependencies', async () => {
-    await build({ inputFolder: 'src/no-dependencies' })
-
-    await expect(readDistPackageJson()).resolves.toEqual({})
-  })
-
-  test('missing dependencies', async () => {
-    await build({ inputFolder: 'src/missing-dependencies' })
-
-    await expect(readDistPackageJson()).resolves.toEqual({})
-  })
-
-  test('dependencies', async () => {
-    await build({ inputFolder: 'src/dependencies' })
-
-    await expect(readDistPackageJson()).resolves.toEqual({
-      dependencies: {
-        koa: '2.0'
-      }
-    })
-  })
-
-  test('base contents', async () => {
-    await build({
-      inputFolder: 'src/dependencies',
-      baseContents: {
-        name: 'my-package',
-        dependencies: {},
-        private: true
-      }
-    })
-
-    await expect(readDistPackageJson()).resolves.toEqual({
+test('base contents', async () => {
+  await build({
+    inputFolder: 'src/dependencies',
+    baseContents: {
       name: 'my-package',
-      dependencies: {
-        koa: '2.0'
-      },
+      dependencies: {},
+      private: true
+    }
+  })
+
+  await expect(readDistPackageJson()).resolves.toEqual({
+    name: 'my-package',
+    dependencies: {
+      koa: '2.0'
+    },
+    private: true
+  })
+})
+
+test('base contents as function', async () => {
+  await build({
+    inputFolder: 'src/base-contents-function',
+    baseContents: pkg => ({
+      name: pkg.name,
+      main: pkg.main.replace('src', 'dist'),
+      dependencies: {},
       private: true
     })
   })
 
-  test('base contents as function', async () => {
-    await build({
-      inputFolder: 'src/base-contents-function',
-      baseContents: pkg => ({
-        name: pkg.name,
-        main: pkg.main.replace('src', 'dist'),
-        dependencies: {},
-        private: true
-      })
-    })
+  await expect(readDistPackageJson()).resolves.toEqual({
+    name: 'test',
+    main: 'dist/index.js',
+    dependencies: {
+      koa: '2.0'
+    },
+    private: true
+  })
+})
 
-    await expect(readDistPackageJson()).resolves.toEqual({
-      name: 'test',
-      main: 'dist/index.js',
-      dependencies: {
-        koa: '2.0'
-      },
-      private: true
-    })
+test('additional dependencies', async () => {
+  await build({
+    inputFolder: 'src/additional-dependencies',
+    additionalDependencies: ['koa-router']
   })
 
-  test('additional dependencies', async () => {
-    await build({
-      inputFolder: 'src/additional-dependencies',
-      additionalDependencies: ['koa-router']
-    })
+  await expect(readDistPackageJson()).resolves.toEqual({
+    dependencies: {
+      koa: '2.0',
+      'koa-router': '7.4'
+    }
+  })
+})
 
-    await expect(readDistPackageJson()).resolves.toEqual({
-      dependencies: {
-        koa: '2.0',
-        'koa-router': '7.4'
-      }
-    })
+test('subpath dependencies', async () => {
+  await build({ inputFolder: 'src/subpath-dependencies' })
+
+  await expect(readDistPackageJson()).resolves.toEqual({
+    dependencies: {
+      uuid: '3.3'
+    }
+  })
+})
+
+test('scoped dependencies', async () => {
+  await build({ inputFolder: 'src/scoped-dependencies' })
+
+  await expect(readDistPackageJson()).resolves.toEqual({
+    dependencies: {
+      '@google-cloud/bigquery': '4.4'
+    }
+  })
+})
+
+test('local dependencies', async () => {
+  await build({
+    inputFolder: 'src/local-dependencies',
+    additionalDependencies: {
+      koa: '2.11.0',
+      'react-calendar': 'file:../react-calendar/react-calendar-v2.13.2.tgz'
+    }
   })
 
-  test('subpath dependencies', async () => {
-    await build({ inputFolder: 'src/subpath-dependencies' })
+  await expect(readDistPackageJson()).resolves.toEqual({
+    dependencies: {
+      koa: '2.11.0',
+      'react-calendar': 'file:../react-calendar/react-calendar-v2.13.2.tgz'
+    }
+  })
+})
 
-    await expect(readDistPackageJson()).resolves.toEqual({
-      dependencies: {
-        uuid: '3.3'
-      }
-    })
+test('unique dependencies from multiple chunks', async () => {
+  const bundle = await rollup({
+    input: [
+      'src/unique-dependencies/app-1.js',
+      'src/unique-dependencies/app-2.js'
+    ],
+    plugins: [
+      generatePackageJson({ inputFolder: 'src/unique-dependencies' })
+    ],
+    external: ['koa']
   })
 
-  test('scoped dependencies', async () => {
-    await build({ inputFolder: 'src/scoped-dependencies' })
-
-    await expect(readDistPackageJson()).resolves.toEqual({
-      dependencies: {
-        '@google-cloud/bigquery': '4.4'
-      }
-    })
+  await bundle.write({
+    dir: 'dist',
+    format: 'cjs'
   })
 
-  test('local dependencies', async () => {
-    await build({
-      inputFolder: 'src/local-dependencies',
-      additionalDependencies: {
-        koa: '2.11.0',
-        'react-calendar': 'file:../react-calendar/react-calendar-v2.13.2.tgz'
-      }
-    })
-
-    await expect(readDistPackageJson()).resolves.toEqual({
-      dependencies: {
-        koa: '2.11.0',
-        'react-calendar': 'file:../react-calendar/react-calendar-v2.13.2.tgz'
-      }
-    })
+  await expect(readDistPackageJson()).resolves.toEqual({
+    dependencies: {
+      koa: '2.0'
+    }
   })
+})
 
-  test('unique dependencies from multiple chunks', async () => {
-    const bundle = await rollup({
-      input: [
-        'src/unique-dependencies/app-1.js',
-        'src/unique-dependencies/app-2.js'
-      ],
-      plugins: [
-        generatePackageJson({ inputFolder: 'src/unique-dependencies' })
-      ],
-      external: ['koa']
-    })
-
-    await bundle.write({
-      dir: 'dist',
-      format: 'cjs'
-    })
-
-    await expect(readDistPackageJson()).resolves.toEqual({
-      dependencies: {
-        koa: '2.0'
-      }
-    })
-  })
-
-  test('assets mixed with chunks', async () => {
-    function addAsset() {
-      return {
-        name: 'add-asset',
-        load() {
-          this.emitFile({
-            type: 'asset',
-            source: 'console.log(\'hey\')',
-            name: 'asset.js'
-          })
-        }
+test('assets mixed with chunks', async () => {
+  function addAsset() {
+    return {
+      name: 'add-asset',
+      load() {
+        this.emitFile({
+          type: 'asset',
+          source: 'console.log(\'hey\')',
+          name: 'asset.js'
+        })
       }
     }
+  }
 
-    const bundle = await rollup({
-      input: 'src/assets/index.js',
-      plugins: [
-        addAsset(),
-        generatePackageJson({ inputFolder: 'src/assets' })
-      ]
-    })
-
-    await bundle.write({
-      dir: 'dist',
-      format: 'cjs'
-    })
-
-    await expect(readDistPackageJson()).resolves.toEqual({})
+  const bundle = await rollup({
+    input: 'src/assets/index.js',
+    plugins: [
+      addAsset(),
+      generatePackageJson({ inputFolder: 'src/assets' })
+    ]
   })
+
+  await bundle.write({
+    dir: 'dist',
+    format: 'cjs'
+  })
+
+  await expect(readDistPackageJson()).resolves.toEqual({})
 })
